94e3cdffd0d7bfa0022ffb13ab96ada957afde4f
unknown <ismartash>
2025-01-18 14:57:39 +0530

    dbchange

--- retrieveimg.py
+++ retrieveimg.py
@@ -9,7 +9,7 @@ load_dotenv()
 client = MongoClient(os.getenv("MONGODB_URI"))
 db = client["vision_ai"]
 collection = db["screenshots"]
-num_images = 5  # no of images to retrieve
+num_images = 3  # no of images to retrieve
 screenshot_data_list = collection.find().sort("timestamp", -1).limit(num_images)
 for index, screenshot_data in enumerate(screenshot_data_list):
     screenshot_binary = screenshot_data["screenshot"]
